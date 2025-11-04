e8cff610dc8f8c6a45a639c4f9704f198e59c7cb
Pacome-grn <pagarnier@eleve.st-charles.eu>
2025-11-04 15:57:20 +0000

    feat: Maîtrise des Objets JS

--- script.js
+++ script.js
@@ -21,7 +21,17 @@ const moi = {
     competences: ["HTML", "CSS"]
 };
  console.log(moi)
-console.log(moi.prenom); // Affiche "VotrePrénom"
+console.log(moi.prenom); // Affiche "je ma'appelle VotrePrénom"
 moi.age = 19
 moi.ville = "Lyon"
 console.log(moi)
+const notes = [12, 15, 9, 18];
+console.log(notes[0]); // Affiche 12 console.log(notes[1]); // Affiche 15
+console.log(notes.length); // Affiche 4
+for (let i = 0; i < notes.length; i++) {
+    console.log("La note à l'indice " + i + " est " + notes[i]);
+}
+const classe = [
+    { prenom: 'Alice', age: 18 },
+    { prenom: 'Bob', age: 17 }
+];
